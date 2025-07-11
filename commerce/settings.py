e60304a7db462f803bec62b5acbdd180f3544a6e
azwerraza <azwerraza01@gmail.com>
2025-07-11 23:45:49 +0500

    � add logging to debug production crash

--- commerce/settings.py
+++ commerce/settings.py
@@ -189,3 +189,19 @@ CKEDITOR_5_CONFIGS = {
         'custom_css': 'css/ckeditor-dark.css',  # ✅ Relative to /static/
     }
 }
+
+import logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
