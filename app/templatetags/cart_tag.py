578b4f725533e1a2bd3d9b7c809c12cb9322712e
azwerraza <azwerraza01@gmail.com>
2025-07-12 15:22:41 +0500

    Cleaned project structure, added cart_tag template tag

---
+++ app/templatetags/cart_tag.py
@@ -0,0 +1,18 @@
+# your_app/templatetags/cart_tag.py
+
+from django import template
+from cart.cart import Cart  # Adjust path if needed
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def cart_item_count(context):
+    request = context['request']
+    cart = Cart(request)
+    return len(cart)
+
+@register.simple_tag(takes_context=True)
+def cart_total_price(context):
+    request = context['request']
+    cart = Cart(request)
+    return cart.get_total_price()
